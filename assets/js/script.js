d45619731bb1bc1ccf6c0f87343bcd28b29e8e65
terintealexandrin18 <>
2023-10-02 18:13:47 +0000

    update the background picture with a compress size

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hellow World!")
+// Variable 
+const rules = document.getElementById('quiz-rules');
+
+
+// Function Hide The Button
+
+// test
+
+function getAlert() {
+    alert("Hello World!");
+}
+
+rules.addEventListener('click', getAlert);
