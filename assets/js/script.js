5c07df41077a6fed0d1fc0a5673e41807d03476b
terintealexandrin18 <>
2023-10-03 19:42:35 +0000

    add buttons for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
-// Variable 
+
+// Variable
+const startQuiz = document.getElementById('start-btn');
 const rules = document.getElementById('quiz-rules');
+const noRules = document.getElementById('close-rules');
 
-
-// Function Hide The Button
-
-// test
-
-function getAlert() {
-    alert("Hello World!");
+//event listeners fot Start Button, 
+startQuiz.addEventListener('click', hideButton, false);
+rules.addEventListener('click', showRules);
+noRules.addEventListener('click', hideRules);
+// Function Hide Buttons
+function hideButton() {
+    this.style.display = 'none';
 }
 
-alert("If you can see me, the JS file has updated.");
+// Function Show Rules
+function showRules() {
+    document.getElementById('quiz-instruction').style.display = 'block';
+}
 
-rules.addEventListener('click', getAlert);
+// Function Hide Rules
+function hideRules() {
+    document.getElementById('quiz-instruction').style.display = 'none';
+}
