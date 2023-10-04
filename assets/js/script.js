66227b9cfce38f8a83c3bc4b1f59ae7f7d75f323
terintealexandrin18 <>
2023-10-04 17:36:59 +0000

    add function to show/hide rules and function to hide the quiz introduction when quiz start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 
 // Variable
-const startQuiz = document.getElementById('start-btn');
+const start = document.getElementById('start-btn');
 const rules = document.getElementById('quiz-rules');
 const noRules = document.getElementById('close-rules');
-
+const questionContainer = document.getElementById('question-container');
+const quizIntroduction = document.getElementById('quiz-header');
 //event listeners fot Start Button, 
-startQuiz.addEventListener('click', hideButton, false);
+
 rules.addEventListener('click', showRules);
 noRules.addEventListener('click', hideRules);
-// Function Hide Buttons
-function hideButton() {
-    this.style.display = 'none';
-}
+start.addEventListener('click', startQuiz);
 
 // Function Show Rules
 function showRules() {
@@ -21,4 +19,20 @@ function showRules() {
 // Function Hide Rules
 function hideRules() {
     document.getElementById('quiz-instruction').style.display = 'none';
+}
+
+// Function start the quiz
+function startQuiz() {
+    start.classList.add('hide');
+    questionContainer.classList.remove('hide');
+    quizIntroduction.classList.add('hide');
+}
+//Function set the nex question
+function setNextQuestion() {
+
+}
+
+//Function Answert
+function chooseAnswer() {
+
 }
